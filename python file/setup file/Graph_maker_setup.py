ed76d250e1957e5f760e7f10642c82323120c66c
tt_umv <60070029@it.kmitl.ac.th>
2017-11-17 16:11:49 +0700

    debug color code

--- python file/setup file/Graph_maker_setup.py
+++ python file/setup file/Graph_maker_setup.py
@@ -4,7 +4,8 @@ import pygal as pg
 from pygal.style import Style
 custom_style = Style(
   background='transparent',
-  colors=('#00723F', '#0B421A', '#00723F', '#EAC784', '#e5a632', '#604C4C', '#BBBBBB', '#AAAAAA'))
+  colors=('#18301e', '#0B421A', '#00723F', '#EAC784', '#e5a632', '#604C4C', '#BBBBBB', '#AAAAAA'),
+  value_colors = '#212320')
 
 def main():
     raw_data = pd.read_csv('../data/starbucks_drinkMenu_expanded.csv')
